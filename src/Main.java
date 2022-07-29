4edda24d583e429363e75e146e0d67debc9017f5
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-07-28 20:12:42 -0500

    Agregar en la clase main
    Se agregó el código correspondiente al llamado del panel para visualizar los emojis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import javax.swing.*;
+
+import static java.lang.Boolean.TRUE;
+
 public class Main {
     public static void main(String[] args) {
+        String entrada = JOptionPane.showInputDialog("Escriba 1 para dibujar rectangulos \n" + "Escriba 2 para dibujar ovalos\n" + "Escriba 3 para dibujar nombre");
+        int opcion = Integer.parseInt(entrada);
 
+        figuras panel = new figuras(opcion);
+        JFrame aplicacion = new JFrame();
+
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aplicacion.add(panel);
+        aplicacion.setSize(1200, 500);
+        aplicacion.setVisible(TRUE);
 
     }
 }
