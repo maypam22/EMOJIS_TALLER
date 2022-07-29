8cb259eb734538b0675a664db65bb25ee35341fd
Lesly <leslyherrera851@gmail.com>
2022-07-28 20:51:48 -0500

    CREACION DE EMOJI
    PROCESO DE LA CREACION DEL EMOJI ENOJADO

--- src/figuras.java
+++ src/figuras.java
@@ -25,6 +25,38 @@ public class figuras extends JPanel {
                 case 4:
 
                     break;
+                case 5:
+                    g.setColor( Color.YELLOW );
+                    g.fillOval( 10, 10, 200, 200 );
+
+                    g.setColor( Color.BLACK );
+                    g.fillArc( 55, 50, 45, 26 ,0,180);
+                    g.fillArc( 133, 50, 45, 26,0,180 );
+
+                    g.fillOval( 55, 65, 30, 30 );
+                    g.fillOval( 135, 65, 30, 30 );
+
+                    g.fillOval( 50, 110, 120, 60 );
+
+                    g.setColor( Color.YELLOW );
+                    g.fillOval( 55, 60, 26, 26 );
+                    g.fillOval( 135, 60, 26, 26 );
+
+                    g.fillOval( 50, 120, 120, 70 );
+
+                    g.fillArc( 50, 55, 55, 30,0,180 );
+                    g.fillArc( 128, 55, 55, 30, 0, 180);
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
             }
         }
 
