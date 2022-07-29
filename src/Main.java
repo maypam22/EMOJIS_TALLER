d8e4c6c0a5a4c78c24223df9f0af5eda953a3cec
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-07-28 20:19:06 -0500

    Agregar opciones
    Se agregó las primeras cuatros opciones que forman parte del menú

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,17 @@ import static java.lang.Boolean.TRUE;
 
 public class Main {
     public static void main(String[] args) {
-        String entrada = JOptionPane.showInputDialog("Escriba 1 para dibujar rectangulos \n" + "Escriba 2 para dibujar ovalos\n" + "Escriba 3 para dibujar nombre");
+        String entrada = JOptionPane.showInputDialog(
+                "Escriba 1 para dibujar emoji enojado \n" +
+                "Escriba 2 para dibujar emoji robot\n" +
+                "Escriba 3 para dibujar emoji carita tierna\n" +
+                "Escriba 4 para dibujar emoji con lengua afuera\n"
+
+
+
+
+
+        );
         int opcion = Integer.parseInt(entrada);
 
         figuras panel = new figuras(opcion);
